b627a4178e32b4d55a7f351c26ae5c537f69a8e0
heim nicolas <heim.nico@gmail.com>
2024-09-13 14:28:54 +0200

    ajustement séparation resources

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 
 # Cms like using quart
 
+
+
+## start
+
+
+````bash
+(venv) (venv) app@debian:/app/src$ python3 ./ep.py 
+2024-09-13 10:43:50,834 root INFO  importing dependencies ...
+2024-09-13 10:43:50,867 root INFO  generating json schema ...
+2024-09-13 10:43:50,867 - INFO - app_context - generating json schema ...
+2024-09-13 10:43:50,877 default INFO  starting application in debug mode ...
+2024-09-13 10:43:50,877 - INFO - ep - starting application in debug mode ...
+2024-09-13 10:43:50,877 asyncio DEBUG Using selector: EpollSelector
+2024-09-13 10:43:50,877 - DEBUG - selector_events - Using selector: EpollSelector
+ * Serving Quart app 'etp0'
+ * Debug mode: True
+ * Please use an ASGI server (e.g. Hypercorn) directly in production
+ * Running on http://127.0.0.1:5000 (CTRL + C to quit)
+2024-09-13 10:43:50,905 asyncio INFO  <Server sockets=(<asyncio.TransportSocket fd=6, family=2, type=1, proto=0, laddr=('127.0.0.1', 5000)>,)> is serving
+2024-09-13 10:43:50,905 - INFO - base_events - <Server sockets=(<asyncio.TransportSocket fd=6, family=2, type=1, proto=0, laddr=('127.0.0.1', 5000)>,)> is serving
+[2024-09-13 10:43:50 +0200] [8403] [INFO] Running on http://127.0.0.1:5000 (CTRL + C to quit)
+2024-09-13 10:43:50,905 hypercorn.error INFO  Running on http://127.0.0.1:5000 (CTRL + C to quit)
+2024-09-13 10:43:50,905 - INFO - logging - Running on http://127.0.0.1:5000 (CTRL + C to quit)
+````
--- src/routes/default.py
+++ src/routes/default.py
@@ -8,80 +8,51 @@ from jinja2 import FileSystemLoader
 from model.data_model import BlogDescriptorEntryPydantic, IBlogDescriptorEntry
 import os
 
+content_type_by_extension = { 
+    '.css' : 'text/css',
+    '.html' : 'text/html',
+    '.js' : 'text/javascript',
+    '.pdf': 'application/pdf'
+}
+
 class DefaultRouteController():
 
 
     @app.route("/", methods=["GET"])
     async def default():
-        return await DefaultRouteController.hello0("index.html")
+        return await DefaultRouteController.templates_route("index.html")
+    
+    @app.route("/services/<service>", methods=["POST"])
+    async def services(service: str):
+        return f"service name : {service}", 200, {'Content-Type': 'text/html'}
 
-    @app.route("/<path:name>", methods=["GET"])
-    async def hello0(name: str):
+    
+    @app.route("/t/<path:name>", methods=["GET"])
+    async def templates_route(name: str):
         
-        from quart import request
-        
-        full_path = os.path.join('../resources', 'templates', name)
         full_template_path = os.path.join('../resources', 'templates', f"{name}.jinja2")
-        
-        
-        _, extension = os.path.splitext(full_path)     
-        
-        return_value = None 
+        full_path = os.path.join('../resources', 'templates', name)
+        _, extension = os.path.splitext(full_path)
 
-        while True:
+        return_value = None
+
+        if os.path.exists(full_template_path):
             
-            if extension == 'jinja2' :
-                return 'not found', 404
+            app.jinja_env.loader = FileSystemLoader(['../resources/templates/'])               
             
-            if os.path.exists(full_template_path):
-                
-                # async def stream_template_generator():
-                    
-                #     co = await stream_template(full_template_path, context={ 'test': 'aa' })
-                #     for value in co:
-                #         yield value
-                
-                # return_value = stream_template_generator()
-                app.jinja_env.loader = FileSystemLoader(['../resources/templates/'])
-                # app.jinja_env.compile_templates(full_template_path)
-                
-                
-                with open('../resources/data.json') as data_file:
-                    data_as_string = "\n".join(data_file.readlines())
-                    data = BlogDescriptorEntryPydantic.model_validate_json(data_as_string)
-                    t_data= IBlogDescriptorEntry(**data.model_dump())
-                    return_value = await stream_template(f'{name}.jinja2', context={"data": t_data})
-                
-                
-                break
+            with open('../resources/data.json') as data_file:
+                data_as_string = "\n".join(data_file.readlines())
+                data = BlogDescriptorEntryPydantic.model_validate_json(data_as_string)
+                t_data= IBlogDescriptorEntry(**data.model_dump())
+                return_value = await stream_template(f'{name}.jinja2', context={"data": t_data, "csrf": "aaaaaaaaaaaaaa"})
 
-            if os.path.exists(full_path):
-                
-                    async def file_content_generator():
-                        with open(full_path, 'rb') as file:
-                            
-                            while True:
-                                data = file.read()
-                                if not data:
-                                    break
-                                
-                                yield data
 
-                    return_value = file_content_generator()
-                                            
-                    break
-
-            # if not any found
-            return 'not found', 404
-            
-        content_type_by_extension = { 
-            '.css' : 'text/css',
-            '.html' : 'text/html',
-            '.js' : 'text/javascript',
-            '.pdf': 'application/pdf'
-        }
+        # if not any found
+        if return_value is None :
+            return f'{name} not found', 404
 
         content_type = None
+
 
         if extension in content_type_by_extension:
             content_type = content_type_by_extension[extension]
@@ -90,6 +61,46 @@ class DefaultRouteController():
 
         
         return return_value, 200, {'Content-Type': content_type}
-    
+
+    @app.route("/r/<path:name>", methods=["GET"])
+    async def resources_route(name: str):
                 
+        full_path = os.path.join('../resources', 'templates', name)
+        _, extension = os.path.splitext(full_path)
+        
+        if extension in [ 'jinja2', 'py' ]:
+            return f'{name} not found', 404
+
+        return_value = None
+
+        if os.path.exists(full_path):
+            
+            async def file_content_generator():
+                with open(full_path, 'rb') as file:
+                    
+                    while True:
+                        data = file.read()
+                        if not data:
+                            break
+                        
+                        yield data
+
+            return_value = file_content_generator()
+                                        
+
+        # if not any found
+        if return_value is None :
+            return f'{name} not found', 404
+  
+                
+        content_type = None
+        
+        _, extension = os.path.splitext(full_path)
+
+        if extension in content_type_by_extension:
+            content_type = content_type_by_extension[extension]
+        else:
+            content_type = "Unknown"
+            
+        return return_value, 200, {'Content-Type': content_type}
             
